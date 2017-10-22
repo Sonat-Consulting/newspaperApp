126b31198dd8e65175eabc836753e7ce2b21534d
Modulus <thejohnskauge@gmail.com>
2017-10-22 11:11:35 +0200

    Restructured code

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import newspaper
+from newspaper.article import ArticleException
 from flask import Flask, jsonify, request
 import logging
 import sys
+import os
 
 app = Flask(__name__)
 
@@ -18,6 +20,8 @@ logger.addHandler(ch)
 
 @app.route("/health", methods=["GET"])
 def health_check():
+    logger.debug(os.environ["LOG_LEVEL"])
+    logger.debug(os.environ["ARTICLE_AMOUNT"])
     return jsonify({"status": "healty"}), 200
 
 
@@ -31,49 +35,76 @@ def get_articles():
 
     logger.info("Fetching articles from %s", url)
 
+    data = extract_articles(url)
+
+    return jsonify(data)
+
+
+def extract_articles(url):
+    """
+    Extracts all articles below environment variable ARTICLE_AMOUNT or 20 
+    :param url: a valid url, with or without http
+    :return: a dictionary containing all elements
+    """
     paper = newspaper.build(str(url), memoize_articles=False)
     logger.info("Amount of articles found: %s", len(paper.articles))
     data = {
-            "total": len(paper.articles),
-            "content": [
+        "total": len(paper.articles),
+        "content": [
 
-                ],
-            "failed": []
-            }
+        ],
+        "failed": []
+    }
     articles = []
     failed = []
     for index, article in enumerate(paper.articles):
-        if index < 20:
-            article.download()
-            article.parse()
-            article.nlp()
+        if "ARTICLE_AMOUNT" in os.environ and index < int(os.environ["ARTICLE_AMOUNT"]) or index < 20:
             try:
-                current_article = {
-                    "title": article.title,
-                    "text": article.text,
-                    "top_image": article.top_image,
-                    "summary": article.summary,
-                    "keywords": article.keywords,
-                    "authors": article.authors,
-                    "movies": article.movies,
-                    "language": article.meta_lang
-                }
-
-                if current_article["text"] and len(current_article) > 0:
-                    articles.append(current_article)
-                else:
-                    failed.append(current_article)
-            except Exception:
+                extract_article(article, articles, failed)
+            except (Exception, ArticleException):
                 logger.error("Failed to parse article, trying to pass remaining articles")
         else:
             break
-
     data["content"] = articles
     data["failed"] = failed
-
     logger.debug("Found {} articles".format(len(articles)))
     logger.debug("Failed parsings {}".format(len(failed)))
-
     logger.info("Retuning data as json")
+    return data
 
-    return jsonify(data)
+
+def extract_article(article, articles, failed):
+    """
+    Extract one article, and append it to the articles list
+    :param article: Current article metadata
+    :param articles: List to append values to
+    :param failed:  List to append failed extractions to
+    :return: nothing, appends to articles or failed list
+    """
+    article.download()
+    article.parse()
+    article.nlp()
+    current_article = {}
+    for attribute, value in article.__dict__.items():
+        if valid_value(value) and str(attribute) != "html":
+            current_article[str(attribute)] = value
+    if "text" in current_article and current_article["text"] and len(current_article["text"]) > 0:
+        articles.append(current_article)
+    else:
+        failed.append(current_article)
+
+
+def valid_value(value):
+    """
+    Check if a value has a valid type in context of converting it to json 
+    :param value: value to check for type
+    :return: True or False
+    """
+    try:
+        if value and type(value) in [int, float, str, list, dict]:
+            return True
+        else:
+            return False
+    except Exception:
+        return False
+
